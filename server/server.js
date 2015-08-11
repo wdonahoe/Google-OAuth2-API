3faa77b7b76d9c32c86e47d5e8a3741b71090b4c
William Donahoe <wdonahoe@udel.edu>
2015-08-10 21:42:39 -0400

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var httpStatus = require('http-status');
+var express 		= require('express');
+var morgan 			= require('morgan');
+var bodyParser 		= require('body-parser');
+var cookieParser 	= require('cookie-parser');
+var session 		= require('express-session');
+var httpStatus 		= require('http-status');
+var mongoose 		= require('mongoose');
 
-var logger = require('./config/logger');
+var logger 			= require('./config/logger');
+var config 			= require('./config/config');
 
-var app = express();
-var port = Number(process.env.PORT || 3000);
+var app 			= express();
+var port 			= process.env.PORT || 3000
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +24,18 @@ app.use(morgan('combined', {
 	}
 }));
 
+
 app.use('/', require('./routes'));
+
+
+mongoose.connect(config.mongo.url);
+var db = mongoose.connection;
+db.once('open', function(open){
+	logger.info("connected to " + db.name + ' at ' + db.host + ' on port ' + db.port);
+});
+db.on('error', function(err){
+	logger.error(err);
+});
 
 
 app.listen(port, function(){
